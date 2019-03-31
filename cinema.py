81f7ff1a1142fe9ddcb1392bfd6b74ec176d42d0
Vaiva Ignataviciute <43579870+VaivaIgnataviciute@users.noreply.github.com>
2019-03-31 15:41:37 +0200

    Add files via upload

---
+++ cinema.py
@@ -0,0 +1,26 @@
+films= {
+         "Finding Dory":[3,5],
+         "Bourne": [18,5],
+         "Tarzan": [18,5],
+         "Ghost Busters": [12,5]
+         }
+
+while True:
+    choice=input("What movie you would like to watch?").strip().title()
+
+    if choice in films:
+        age=int(input("How old are you?").strip())
+
+        if age >= films[choice][0]: #check user age and if the user choices maches in dictionary
+
+            if films[choice][1] >0:
+              print("Enjoy the film! There are {} seats left".format(films[choice][1]))
+              films[choice][1]=films[choice][1]-1
+            else:print("Sorry, we are sold out")
+            
+            
+        else:
+            print("You are too young to see that film")          
+    
+    else:
+        print("We don't have that film..")
